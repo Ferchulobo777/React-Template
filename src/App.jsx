5814fce3a799037487c9ab0a985b24e0a41c65c0
Fernando Rodriguez <ferchulobo2015@gmail.com>
2023-03-08 08:20:56 -0300

    React-Template

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+
+const App = () => {
+  return (
+    <div className="bg-neutral-800 h-screen flex flex-col justify-center items-center p-10 text-white">
+      
+    </div>
+  );
+};
+
+export default App;
